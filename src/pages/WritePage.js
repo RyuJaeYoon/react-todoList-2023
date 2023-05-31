5a29cf7d72cb4635c7caf1777bca46a9c8820177
RyuJaeYoon <wodbs9505@gmail.com>
2023-05-31 21:52:23 +0900

    snakcbar

---
+++ src/pages/WritePage.js
@@ -0,0 +1,11 @@
+export default function WritePage() {
+    return (
+      <>
+        <div className="felx-1 flex justify-center items-center">
+          <div>
+            <span>글쓰기 페이지</span>
+          </div>
+        </div>
+      </>
+    );
+  }
