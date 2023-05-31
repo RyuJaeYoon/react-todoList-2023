58c8a2eaa989ceaef1fe9a1c981f28842810b54f
RyuJaeYoon <wodbs9505@gmail.com>
2023-05-31 21:25:42 +0900

    분리작업

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import { AppBar, Toolbar } from "@mui/material";
+import {
+  NavLink,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
+import MainPage from "./MainPage";
+import Sub1Page from "./Sub1Page";
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <AppBar position="static">
         <Toolbar>
           <div className="flex-1"></div>
           <div className="font-bold">MY NOTE</div>
-          <div className="flex-1"></div>
+          <div className="flex-1 flex justify-end">
+            {location.pathname != "/sub1" && (
+              <NavLink to="/sub1">서브1</NavLink>
+            )}
+            {location.pathname == "/sub1" && <NavLink to="/main">메인</NavLink>}
+          </div>
         </Toolbar>
       </AppBar>
+
+      <Routes>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/sub1" element={<Sub1Page />} />
+        <Route path="/*" element={<Navigate to="/main" />} />
+      </Routes>
     </>
   );
 }
